50cbc5e1341d3cef19188d5370e13edb40929b30
Pablio Richardy <64492520+pablioRichardy@users.noreply.github.com>
2024-06-08 15:15:17 -0300

    wip: arrumando init.sh se excluindo

--- trocaTroca/api/init.sh
+++ trocaTroca/api/init.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+# Caminho para o diretório do script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Caminho para a pasta da API dentro do contêiner
 API_DIR=/application/trocaTroca/api
 
 # URL do repositório da API
 REPO_URL=https://github.com/pablioRichardy/trocaTrocaAPI.git
 
-# Verificar se o arquivo de marcação existe
-if [ ! -f $API_DIR/.initialized ]; then
+# Função para clonar o repositório e instalar dependências
+initialize_api() {
   apk update && apk add git
   echo "Clonando o repositório da API..."
-  rm -rf $API_DIR/*  # Remove todo o conteúdo da pasta API
-  git clone $REPO_URL $API_DIR
-  touch $API_DIR/.initialized
+  rm -rf "$API_DIR"/*  # Remove todo o conteúdo da pasta API
+  git clone "$REPO_URL" "$API_DIR"
+  touch "$API_DIR/.initialized"
   echo "Repositório clonado e arquivo de marcação criado."
+
+  cd "$API_DIR" || exit
+  npm install
+}
+
+# Verificar se o arquivo de marcação existe
+if [ ! -f "$API_DIR/.initialized" ]; then
+  initialize_api
 fi
 
-# Verificar se package.json existe e instalar dependências
-if [ -f $API_DIR/package.json ]; then
-  cd $API_DIR
-  npm install
+# Verificar se o arquivo do script foi excluído e recriá-lo se necessário
+if [ ! -f "$SCRIPT_DIR/init.sh" ]; then
+  echo "Recriando o script init.sh..."
+  cp "$0" "$SCRIPT_DIR/init.sh"
+  echo "Script init.sh recriado."
 fi
 
 # Comando principal do contêiner
